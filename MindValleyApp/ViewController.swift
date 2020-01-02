10d5cfef361b20b81898107ce4b85a2fa97789c9
Codesworth <codezworth@gmail.com>
2020-01-02 12:09:16 +0000

    Initial Commit

---
+++ MindValleyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MindValleyApp
+//
+//  Created by Shadrach Mensah on 02/01/2020.
+//  Copyright © 2020 Shadrach Mensah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
